10a4f27794e22a31b43de788b5a023e3cac07d70
alexlyee <alexalternateemail@gmail.com>
2023-07-09 23:30:41 -0400

    cascading changes from new understanding of project needs

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -4,16 +4,22 @@ from datetime import datetime
 import xarray as xr
 import numpy as np
 
-def preprocess_era5_data(variable:str, interpolate=1.25, get_from='./', save_to='./'):
+def preprocess_era5_data(variable, transform=None, interpolate=1.25, get_from='./', save_to='./'):
     '''
         loads data from {get_from}data.nc, 
         interpolates at 1.25 degrees by default, 
         converts temp to celsius, 
         and saves dataframe as
-        long | lat | time | var
+        long | lat | time | [variable | s...]
 
-        variable can be: '2m_temperature'
+        variables can be: '2m_temperature'
+
+        transform if not nonetype (str or list thereof) will transform variable name/s to something else.
+
+        averages daily data.
     '''
+    # for some reason era5 downloads to a variable with a different name than the one they ask for.
+
     # load netCDF file
     data = xr.open_dataset(f'{get_from}data.nc')
 
@@ -42,6 +48,14 @@ def preprocess_era5_data(variable:str, interpolate=1.25, get_from='./', save_to=
     # Reset the index to make Lat, Lon, and Time columns
     df = df.reset_index()
 
+    # Average to daily values
+
+
+    # Transform variable names
+    if isinstance(transform, str): transform = [transform]
+    if isinstance(transform, list):
+        pass
+    
     # Convert tmp to celsius
     if variable == '2m_temperature':
         df['t2m'] = df['t2m'] - 273.15
@@ -52,13 +66,9 @@ def preprocess_era5_data(variable:str, interpolate=1.25, get_from='./', save_to=
     print(f"dataframe {variable} saved as csv with columns:\t{', '.join(df.columns)}")
     return True
 
-def check_ipcc_tmp(df):
-    pass
+################### GCM
 
-
-################### IPCC
-
-def preprocess_ipcc_data(variable:str, interpolate=1.25, get_from='./', save_to='./'):
+def preprocess_gcm_data(variable:str, interpolate=1.25, get_from='./', save_to='./'):
     pass
 
 ################### GSOD
